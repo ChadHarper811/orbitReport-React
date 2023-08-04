4beab4345ef59bd1f7028f7b5fc0db54a5bfafda
Chad Harper <Hotshott55@gmail.com>
2023-08-03 22:38:19 -0500

    created banner comp, imported styling.css to all comps, and added banner to apps.js

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import './styling.css';
+
+export default function Banner(){
+    return(
+        <div>
+            <header><h1>Orbit Report</h1></header>
+        <p>Click on the orbit type buttons to see the satellites in those orbits</p>
+        </div>
+        
+    )
+}
