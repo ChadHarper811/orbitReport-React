a6c66e14c1b9ea4d60a2dcf99c67eff4972e1449
Chad Harper <Hotshott55@gmail.com>
2023-08-06 13:26:27 -0500

    added code from step 4 update buttons.js

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,19 @@
 import './styling.css';
+import satData from './satData';
 
-const Buttons = () => {
+
+const Buttons = (filterByType, setSat, displaySats) => {
+  
   return (
     <div className='flex-container'>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {displaySats.map((sat, id) => {
+        return (
+          <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
       </div>
   );
 };
