3c51c94ac541cc1fc74f8f86eabe6fcb9d70394f
Chad Harper <Hotshott55@gmail.com>
2023-08-09 22:43:57 -0500

    tried fixing buttons code, still not functional

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -3,17 +3,15 @@ import satData from './satData';
 
 
 const Buttons = (filterByType, setSat, displaySats) => {
-  const firstButton = {displaySats.map((sat, id) => {
-    return (
-      <button onClick={() => filterByType(sat)} key={id}>
-        {sat} Orbit
-      </button>
-    );
-  })}
-
   return (
     <div className='flex-container'>
-      {firstButton}
+      {displaySats?.map((sat, id) => {
+        return (
+          <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })}
       <button onClick={() => setSat(satData)}>All Orbits</button>
       </div>
   );
